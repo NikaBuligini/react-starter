8d86f97583506081b77cd22dd3c2b72c12f8c67f
Nikoloz Buligini <nikoloz.buligini@emoney.ge>
2017-09-05 17:19:15 +0400

    adds enzyme test

--- src/containers/home/index.test.js
+++ src/containers/home/index.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { wrapWithIntl } from '../../utils/intl-enzyme-test-helper';
 import Home from './index';
 
-test('renders Home component', () => {
-  const component = renderer.create(<Home />);
-  const tree = component.toJSON();
-  expect(tree).toMatchSnapshot();
+describe('<Home />', () => {
+  it('should render the Home page', () => {
+    const component = renderer.create(wrapWithIntl(<Home />, 'en'));
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
---
+++ src/containers/notFoundPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { FormattedMessage } from 'react-intl';
+
+import messages from './messages';
+
+const NotFound = () => (
+  <article>
+    <h1>
+      <FormattedMessage {...messages.header} />
+    </h1>
+  </article>
+);
+
+export default NotFound;
